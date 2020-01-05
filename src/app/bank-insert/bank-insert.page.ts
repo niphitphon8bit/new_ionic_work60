3e915de5dda652ab72e4084231eb67a0b163feda
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-01-06 03:44:31 +0700

    add new service for bank insert get data from real database 10.80.39.17 using API controller bank-insert.service.ts , bank.service.ts modify in bank insert page and bank page to show correctly
    Signed-off-by: niphitphon8bit <niphitphon_nt@hotmail.com>

--- src/app/bank-insert/bank-insert.page.ts
+++ src/app/bank-insert/bank-insert.page.ts
@@ -1,6 +1,8 @@
 import { NavParams, ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Location } from "@angular/common";
+import { BankInsertService } from './../service/bank-insert.service';
+
 @Component({
   selector: 'app-bank-insert',
   templateUrl: './bank-insert.page.html',
@@ -16,7 +18,8 @@ export class BankInsertPage implements OnInit {
   constructor(
     private location: Location,
     private NavParams: NavParams,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private BankInsertPage: BankInsertService
   ) {
     this.balance_name = "";
     this.name = "";
@@ -24,18 +27,30 @@ export class BankInsertPage implements OnInit {
     this.status = "true";
   }
 
-  public closeModal(){
+  public db_bank_bases: any = null;
+
+  public closeModal() {
     this.modalCtrl.dismiss({
       'dismissed': true
     });
   }
 
   ngOnInit() {
+    this.get_all_bank_base();
   }
 
-  back(){
+  back() {
     this.location.back();
   }
+
+  get_all_bank_base() {
+    this.BankInsertPage.get_all_bank_base_data().subscribe((res) => {
+      this.db_bank_bases = res;
+      console.log(this.db_bank_bases);
+      // this.set_bank_status();
+    })
+  }
+
 
   insert_bank() {
     this.modalCtrl.dismiss({
--- src/app/service/bank.service.ts
+++ src/app/service/bank.service.ts
@@ -10,8 +10,28 @@ export class BankService {
     private http: Http
   ) { }
 
+  get_all_bank_base_data() {
+    return this.http.get(`https://10.80.39.17/TSP60/Thepd-nu/index.php/tr/API/Ionic_bank/get_all_bank_base`).map((res) => res.json());
+  }
+
   get_all_bank_data() {
-    return this.http.get("http://localhost:3000/api/tr_bank").map((res) => res.json());
+    return this.http.get("https://10.80.39.17/TSP60/Thepd-nu/index.php/tr/API/Ionic_bank/get_all").map((res) => res.json());
+  }
+
+  bank_delete(ba_id) {
+    console.log(`https://10.80.39.17/TSP60/Thepd-nu/index.php/tr/API/Ionic_bank/delete_bank/${ba_id}`)
+    return this.http.delete(`https://10.80.39.17/TSP60/Thepd-nu/index.php/tr/API/Ionic_bank/delete_bank/${ba_id}`)
+      .subscribe(
+        result => console.log(result),
+        err => console.error(err)
+      );
+  }
+
+  bank_insert(bank: Object) {
+    return this.http.post(`https://10.80.39.17/TSP60/Thepd-nu/index.php/tr/API/Ionic_bank/bank_insert`, bank).subscribe(
+      result => console.log(result),
+      err => console.error(err)
+    );;
   }
 
 }
