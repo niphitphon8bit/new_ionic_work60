9ac911dd7b7346cd0256e975a4677255716e81b0
Sanpakorn Suttirattanaporn <60160180@go.buu.ac.th>
2020-01-14 13:34:01 +0700

    add ,
    Signed-off-by: Sanpakorn Suttirattanaporn <60160180@go.buu.ac.th>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,7 +58,7 @@ const routes: Routes = [
   {
     path: 'basedata/expert-type/expert-type-insert',
     loadChildren: () => import('./expert-type-insert/expert-type-insert.module').then( m => m.ExpertTypeInsertPageModule)
-  }  {
+  },  {
     path: 'basedata/food-type/food-insert',
     loadChildren: () => import('./food-insert/food-insert.module').then( m => m.FoodInsertPageModule)
   },
